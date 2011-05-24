25bef781bf96b5fe92a196f45a41b684c43a49c6
PAVANKUMARSEGGOJU <pavan@futurenow.biz>
2011-05-24 12:16:36 +0530

    password encryptions

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'http://rubygems.org'
 gem 'rails', '3.0.6'
 
 gem 'sqlite3'
+gem 'rake', '0.8.7'
 
 group :development do
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,46 @@
+require 'digest'
 class User < ActiveRecord::Base
-attr_accessor :name,:email
-
+attr_accessor :password
+attr_accessible :name, :email, :password, :password_confirmation
 email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+validates :name, :presence => true,
+:length => { :maximum => 50 }
+validates :email, :presence => true,
+:format => { :with => email_regex },
+:uniqueness => { :case_sensitive => false }
 
-# validates :name, :presence=>true,:length => {maximum=>15}
-# validates :email, :presence=>true,:format => { :with => email_regex},:uniqueness => {case_sensitive => false}
+validates :password, :presence => true,
+:confirmation => true,
+:length => { :within => 6..40 }
+
+before_save :encrypt_password
+
+def has_password?(submitted_password)
+encrypted_password == encrypt(submitted_password)
 end
 
-# == Schema Information
-#
+private
+	def encrypt_password
+		self.salt = make_salt if new_record?
+		self.encrypted_password = encrypt(password)
+	end
+	
+	def encrypt(string)
+	secure_hash("#{salt}--#{string}")
+	end
+
+	def make_salt
+	secure_hash("#{Time.now.utc}--#{password}")
+	end
+
+	def secure_hash(string)
+	Digest::SHA2.hexdigest(string)
+	end
+end
+
+
+
+#schema information
 # Table name: users
 #
 #  id         :integer         not null, primary key
