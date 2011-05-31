f80f094acedc7baf895377d80724d2c2a2b9b033
PAVANKUMARSEGGOJU <pavan@futurenow.biz>
2011-05-31 12:07:09 +0530

    pavan commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,6 +39,12 @@ user = find_by_id(id)
 (user && user.salt == cookie_salt) ? user : nil
 end
 
+def feed
+# This is preliminary. See Chapter 12 for the full implementation.
+Micropost.where("user_id = ?", id)
+end
+
+
 
 private
 	def encrypt_password
